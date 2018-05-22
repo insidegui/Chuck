3337c11d1c03c89bdd5f9023785d53c7f3991433
Guilherme Rambo <insidegui@gmail.com>
2018-05-22 19:56:26 -0300

    Created target for ChuckCore, moved unit test resources to ChuckCoreTests

---
+++ ChuckCore/ChuckCore.h
@@ -0,0 +1,19 @@
+//
+//  ChuckCore.h
+//  ChuckCore
+//
+//  Created by Guilherme Rambo on 22/05/18.
+//  Copyright © 2018 Guilherme Rambo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChuckCore.
+FOUNDATION_EXPORT double ChuckCoreVersionNumber;
+
+//! Project version string for ChuckCore.
+FOUNDATION_EXPORT const unsigned char ChuckCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChuckCore/PublicHeader.h>
+
+
---
+++ ChuckCoreTests/ChuckCoreTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChuckCoreTests.swift
+//  ChuckCoreTests
+//
+//  Created by Guilherme Rambo on 22/05/18.
+//  Copyright © 2018 Guilherme Rambo. All rights reserved.
+//
+
+import XCTest
+@testable import ChuckCore
+
+class ChuckCoreTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
