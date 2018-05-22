b76d346488106b2a4f2ce14db24408892c1a751a
Guilherme Rambo <insidegui@gmail.com>
2018-05-22 20:28:24 -0300

    Implemented test for search results parsing with results

--- ChuckCoreTests/ChuckCoreTests.swift
+++ ChuckCoreTests/ChuckCoreTests.swift
@@ -16,5 +16,23 @@ class ChuckCoreTests: XCTestCase {
     func testBundleIntegrity() throws {
         try TestResource.all.forEach { _ = try Bundle.testBundle.fetch(resource: $0) }
     }
+
+    func testSearchResponseWithResultsParsing() throws {
+        let data = try Bundle.testBundle.fetch(resource: .search)
+
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let response = try decoder.decode(SearchResponse.self, from: data)
+
+        let firstJoke = response.result.first
+        XCTAssertNotNil(firstJoke)
+
+        XCTAssert(response.total == 8)
+        XCTAssert(response.result.count == 8)
+        XCTAssert(firstJoke?.id == "RDCtS4GjQpmwByA3ytBs2A")
+        XCTAssert(firstJoke?.value == "I just downloaded the new \"Roundhouse Kick\" app for my iPhone and now my screen is cracked and the phone does not work. Damn you, Chuck Norris.")
+        XCTAssert(firstJoke?.url.absoluteString == "https://api.chucknorris.io/jokes/RDCtS4GjQpmwByA3ytBs2A")
+        XCTAssert(firstJoke?.iconUrl.absoluteString == "https://assets.chucknorris.host/img/avatar/chuck-norris.png")
+    }
     
 }
