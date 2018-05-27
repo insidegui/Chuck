bf3aafa559451009e375a19295a1f4eff5d89d30
Guilherme Rambo <insidegui@gmail.com>
2018-05-27 14:40:08 -0300

    Implemented observables for API calls

--- ChuckCore/Networking/ChuckAPIClient.swift
+++ ChuckCore/Networking/ChuckAPIClient.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 
 public enum APIResult<T> {
     case success(T)
@@ -16,8 +17,48 @@ public enum APIResult<T> {
 
 public final class ChuckAPIClient {
 
+    private struct Constants {
+        static let maxRetryCount = 3
+    }
+
+    public enum APIError: Error {
+        case requestGeneration
+    }
+
     private lazy var session: URLSession = {
         return URLSession(configuration: .default)
     }()
+
+    private func observable<T: Codable>(for endpoint: ChuckAPIEndpoint<T>, responseType: T.Type) -> Observable<T> {
+        guard let request = endpoint.request else {
+            return Observable.error(APIError.requestGeneration)
+        }
+
+        return session.rx.data(request: request).map { data in
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(responseType, from: data)
+        }
+    }
+
+    public lazy var random: Observable<Joke> = {
+        return self.observable(for: .random, responseType: Joke.self)
+    }()
+
+    public func random(with category: Category) -> Observable<Joke> {
+        return observable(for: .random(with: category.name), responseType: Joke.self)
+    }
+
+    public func random(with categoryName: String) -> Observable<Joke> {
+        return observable(for: .random(with: categoryName), responseType: Joke.self)
+    }
+
+    public lazy var categories: Observable<[ChuckCore.Category]> = {
+        return self.observable(for: .categories, responseType: [ChuckCore.Category].self)
+    }()
+
+    public func search(with term: String) -> Observable<SearchResponse> {
+        return observable(for: .search(with: term), responseType: SearchResponse.self)
+    }
 
 }
